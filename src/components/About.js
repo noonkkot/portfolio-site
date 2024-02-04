e910b90d91e7dc53b941a76305e770585d51b88d
noonkkot <oyatnunkkot@gmail.com>
2024-02-04 16:35:40 +0900

    pageUpdate

--- src/components/About.js
+++ src/components/About.js
@@ -37,17 +37,17 @@ function About() {
           Q. 프론트엔드 개발자로서의 목표는?<br/> A. 읽기 쉬운 코드로<br /> 유연한 협업이 가능하도록! <br /> 사용자의 편의성을 고려해<br /> 직관적인 웹 경험이 가능하도록!<br /> 웹 표준과 웹 접근성을 충족하는 웹 개발을 목표로 합니다.<br /> 업데이트는 필수죠! <br />리팩토링을 통해 코드를 개선하고 <br />더 나은 사용자 경험을 <br />지향합니다.
         </p>
       </div>
+
       <div className="aboutSecondPage">
         <div className="about-imgWrap">
           <img className="about-img" src={process.env.PUBLIC_URL + 'images/about.jpg'} alt="어바웃 이미지" />
         </div>
         <div className="about-txt">
-          <h2>💡</h2>
+          <h2>
+            안녕하세요!<br />
+            프론트엔드 개발자 이설화입니다.<br />
+          </h2>
           <div>
-            <p>
-              안녕하세요!<br />
-              프론트엔드 개발자 이설화입니다.<br />
-            </p>
             <p>
               웹 접근성과 웹 표준을 고려해<br />
               리액트로 웹 페이지 만드는 일에 흥미를 느낍니다.<br />
